e95a6f10c5faf486078cf293b50673b6a6acd94c
nicomm2002 <nicolascir.97@gmail.com>
2025-03-19 14:37:53 +0100

    created my first BL App!

--- main.py
+++ main.py
@@ -10,7 +10,11 @@ print("my path to t1w is ", config["t1"])
 # Ejecutar app.py con el argumento del archivo de imagen
 subprocess.run(['python', 'app.py', config["t1"]])
 
+#Configurar el repositorio en tu perfil
+subprocess.run(['git', 'remote', 'remove', 'origin'])
+subprocess.run(['git', 'remote', 'add', 'origin', 'https://github.com/nicomm2002/app-helloworld.git'])
+
 # Añadir cambios al repositorio de Git
 subprocess.run(['git', 'add', '.'])
 subprocess.run(['git', 'commit', '-m', 'created my first BL App!'])
-subprocess.run(['git', 'push'])
+subprocess.run(['git', 'push', '-u', 'origin', 'main'])
