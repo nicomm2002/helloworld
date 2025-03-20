31e8933de051f5fbfd1d70fc8cda3f010917c57d
Nicolas Martinez <nicolascir.97@gmail.com>
2025-03-20 12:29:13 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,35 +1,10 @@
 #!/usr/bin/env python
+
 import sys
 import nibabel as nib
-import os
 
-# Imprimir la versión de nibabel
-print(nib.__version__)
-
-# Verificar que se haya proporcionado un argumento
-if len(sys.argv) < 2:
-    print("Error: No se proporcionó un archivo de imagen.")
-    sys.exit(1)
-
-# Verificar que el archivo existe
-file_path = sys.argv[1]
-print(f"Ruta del archivo de imagen: {file_path}")
-
-# Verificar si el archivo tiene un punto final adicional y eliminarlo
-if file_path.endswith('.'):
-    file_path = file_path[:-1]
-    print(f"Ruta del archivo corregida: {file_path}")
-
-if not os.path.exists(file_path):
-    print(f"Error: El archivo '{file_path}' no existe.")
-    sys.exit(1)
-
-# Cargar la imagen y guardar la cabecera en output.txt
-try:
-    img = nib.load(file_path)
-    with open("output.txt", "w") as f:
-        f.write(str(img.header))
-    print(f"Cabecera de la imagen guardada en 'output.txt'.")
-except Exception as e:
-    print(f"Error al cargar la imagen: {e}")
-    sys.exit(1)
+#just dump input image header to output.txt
+img=nib.load(sys.argv[1])
+f=open("output.txt", "w")
+f.write(str(img.header))
+f.close()
