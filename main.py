9d29324d8584cb0449d541520c99e588281dc56a
Nicolas Martinez <nicolascir.97@gmail.com>
2025-03-21 10:54:18 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,22 +1,10 @@
-import json
-import subprocess
-import os
+#!/usr/bin/env python
 
-try:
-    # Cargar config.json
-    with open('config.json') as f:
-        config = json.load(f)
+import sys
+import nibabel as nib
 
-    print("Mi ruta al archivo EEG es ", config["eeg"])
-
-    # Ejecutar app.py con el argumento del archivo de datos EEG
-    subprocess.run(['python', 'app.py', config["eeg"]])
-
-except FileNotFoundError as e:
-    print(f"Error: {e}")
-except json.JSONDecodeError as e:
-    print(f"Error al cargar el archivo JSON: {e}")
-except KeyError as e:
-    print(f"Error: La clave {e} no se encuentra en config.json")
-except Exception as e:
-    print(f"Ocurrió un error: {e}")
+#just dump input image header to output.txt
+img=nib.load(sys.argv[1])
+f=open("output.txt", "w")
+f.write(str(img.header))
+f.close()
